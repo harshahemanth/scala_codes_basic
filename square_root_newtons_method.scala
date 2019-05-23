354eea23510643f1deea072e8a79207a06b07b91
harshahemanth <chundurihemanth1997@gmail.com>
2019-05-23 14:33:58 +0530

    Much more neat version

--- square_root_newtons_method.scala
+++ square_root_newtons_method.scala
@@ -1,13 +1,17 @@
-def absolute(x: Double) = if (x>=0) x else -x
+def absolute(x: Double) = if (x >= 0) x else -x
 
-def doesItSuffice(guess: Double, x: Double) =
-  absolute(guess * guess - x) /x < 0.001
+def sqrt(x: Double) = {
 
-def betterPerform(guess: Double, x: Double) =
-  (guess + x / guess)/2
+  def doesItSuffice(guess: Double) =
 
-def sqrt(x: Double) = sqrtIter(1.0, x)
+    absolute(guess * guess - x) / x < 0.001
 
-def sqrtIter(guess:Double, x: Double): Double=
-  if(doesItSuffice(guess, x)) guess
-  else sqrtIter(betterPerform(guess, x), x)
+  def betterPerform(guess: Double) =
+    (guess + x / guess) / 2
+
+  def sqrtIter(guess: Double): Double =
+    if (doesItSuffice(guess)) guess
+    else sqrtIter(betterPerform(guess))
+
+  sqrtIter(1.0)
+}
